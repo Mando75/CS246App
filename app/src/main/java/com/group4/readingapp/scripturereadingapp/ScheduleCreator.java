c0497577aecd80061eb2d2821f859718c64331f3
Bryan <starwarsgeek0975@gmail.com>
2017-02-21 19:26:13 -0700

    Added min date to the endDatePicker

--- app/src/main/java/com/group4/readingapp/scripturereadingapp/ScheduleCreator.java
+++ app/src/main/java/com/group4/readingapp/scripturereadingapp/ScheduleCreator.java
@@ -12,5 +12,8 @@ public class ScheduleCreator extends AppCompatActivity {
         setContentView(R.layout.activity_schedule_creator);
         DatePicker startDate = (DatePicker) findViewById(R.id.startDatePicker);
         startDate.setMinDate(System.currentTimeMillis() - 1000);
+        DatePicker endDate = (DatePicker) findViewById(R.id.endDatePicker);
+        // min end date is 1 week
+        endDate.setMinDate(System.currentTimeMillis() + 604800000);
     }
 }
