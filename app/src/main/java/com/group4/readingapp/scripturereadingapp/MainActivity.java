7f36b66654edb9902ec9941149908076bcee287c
Bryan <starwarsgeek0975@gmail.com>
2017-02-21 19:01:15 -0700

    Worked on the basic UI of the home page and schedule creator activities. Planning on merging with master

--- app/src/main/java/com/group4/readingapp/scripturereadingapp/MainActivity.java
+++ app/src/main/java/com/group4/readingapp/scripturereadingapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.group4.readingapp.scripturereadingapp;
 
-import android.support.v7.widget.CardView;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void createSchedule(View view) {
+        Intent intent = new Intent(MainActivity.this, ScheduleCreator.class);
+        startActivity(intent);
     }
 }
